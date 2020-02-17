cee10ea98862bded3ea6fde40dd4dceefd88d4d0
Sebastian Uribe <suribe@gmail.com>
2020-02-17 18:36:46 +0100

    Use compiler-included vue

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  }
 };
