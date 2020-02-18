01efb94aa596eaa73c8a475a5d05202ddfabba87
Sebastian Uribe <suribe@gmail.com>
2020-02-18 14:47:36 +0100

    Clean up key control code

--- src/js/controls.js
+++ src/js/controls.js
@@ -9,27 +9,31 @@ function dir_to_action(dir){
 }
 var animate = false;
 
+const KeyCodeLeft = 37;
+const KeyCodeUp = 38;
+const KeyCodeRight = 39;
+const KeyCodeDown= 40;
+
+function key2Action(keyCode) {
+  switch (keyCode) {
+    case KeyCodeLeft: return dir_to_action(dir.LEFT);
+    case KeyCodeUp: return dir_to_action(dir.UP);
+    case KeyCodeRight: return dir_to_action(dir.RIGHT);
+    case KeyCodeDown: return dir_to_action(dir.DOWN);
+  }
+  return undefined;
+}
+
+function isLightBoxInactive() {
+  return document.querySelector(".lightbox.active") == null;
+}
+
 export function key_callback(e) {
-  var tmp;
-  if (animate){
+  if (animate) {
     return
   }
-  switch (e.keyCode) {
-        case 37:
-            tmp = dir_to_action(dir.LEFT);
-            break;
-        case 38:
-            tmp = dir_to_action(dir.UP);
-            break;
-        case 39:
-            tmp = dir_to_action(dir.RIGHT);
-            break;
-        case 40:
-            tmp = dir_to_action(dir.DOWN);
-            break;
-  }
-  var ret = 1;
-  if (tmp != undefined && document.querySelector(".lightbox.active") == null){
-    ret = machine.step(tmp);
+  var action = key2Action(e.keyCode);
+  if (action != undefined && isLightBoxInactive()) {
+     machine.step(action);
   }
 }
