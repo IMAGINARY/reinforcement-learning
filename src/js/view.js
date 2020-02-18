7ccacba9c5ef3f22598cc0de12750358e619d86c
Sebastian Uribe <suribe@gmail.com>
2020-02-18 09:54:14 +0100

    Temporary disable katex

--- src/js/view.js
+++ src/js/view.js
@@ -691,7 +691,7 @@ var app = new Vue({
     },
   }
 })
-
+/*
 function renderLatex() {
   // (1-lr) * Q[state, action] + lr * (reward + gamma * np.max(Q[new_state, :])
   const expression = `Q(s,a)\\leftarrow${(1-machine.lr).toFixed(2)}Q(s,a)+${machine.lr.toFixed(2)}(reward + ${machine.df.toFixed(2)}\\max_{a'}(Q(s_{new}, a'))`;
@@ -699,3 +699,4 @@ function renderLatex() {
   katex.render(expression, baseNode, { displayMode: true } );
 }
 renderLatex();
+*/
