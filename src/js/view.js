cbec86577d5cdd826603cafcb9115fd58c2556e1
Sebastian Uribe <suribe@gmail.com>
2020-02-18 09:52:43 +0100

    Move State Manager to separate file

--- src/js/view.js
+++ src/js/view.js
@@ -5,10 +5,10 @@ import katex from 'katex';
 import { TimelineLite } from "gsap";
 //import VueSlider from 'vue-slider-component'
 
-import { Texts } from "./language.js";
 import { machine, maze, tile, dir } from "./rl.js";
 import { key_callback } from "./controls.js";
 import { defer } from './utils.js';
+import { StateMgr } from './state-manager.js';
 
 document.addEventListener('keydown', key_callback);
 
@@ -699,42 +699,3 @@ function renderLatex() {
   katex.render(expression, baseNode, { displayMode: true } );
 }
 renderLatex();
-
-// ----------------------------------------------------------------------------
-// ------------------------------ StateMgr ------------------------------------
-// ----------------------------------------------------------------------------
-
-const StateMgr = {
-  init: {
-    onEnterState: function () {
-      var lightText = Texts.intro;
-      this.$lightbox.popup(lightText, ["next"]).then((r) => this.changeState("local"));
-    },
-  },
-  local: {
-    components: ["local", "navi", "score"],
-    navigation: {
-      "reset robot": () => machine.reset_machine(),
-      "continue": null,
-    },
-    onEnterState: function () {
-      this.navigation.continue = () => this.changeState("global");
-      var lightText = Texts.localIntro;
-      this.$lightbox.popup(lightText, ["next"]);
-    },
-  },
-  global: {
-    components: ["global", "sliders", "plot", "navi", "score"],
-    navigation: {
-      "run 1 episode!": () => machine.run(1),
-      "run 100 episodes!": () => machine.run(100),
-      "auto step!": () => machine.auto_step(),
-      "greedy step!": () => machine.greedy_step(),
-      "reset machine": () => machine.reset_machine(),
-    },
-    onEnterState: function () {
-      var lightText = Texts.globalIntro;
-      this.$lightbox.popup(lightText, ["continue"]);
-    },
-  }
-};
