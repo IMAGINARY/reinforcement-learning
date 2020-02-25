cee4f50566fd78eb9ba31f4cb975f33983a673e1
Sebastian Uribe <suribe@gmail.com>
2020-02-25 16:44:52 +0100

    Use coordinate iterator for creating maze layer

--- src/js/map.js
+++ src/js/map.js
@@ -1,8 +1,7 @@
-import Vue from 'vue';
 import Konva from 'konva';
 
-import { maze, tile, machine } from "./rl.js";
-import { hexToRgb, rgbToHex } from './color-utils';
+import { maze, machine } from "./rl.js";
+import { rgbToHex } from './color-utils';
 
 export const TileStrokeColor = "#DDDDDD";
 const TileFogColor = "#303030";
@@ -41,7 +40,6 @@ function createMatrixFromMaze(maze) {
   return matrix;
 }
 
-
 export class MapView {
   constructor(containerId, machine, maze, tileSize) {
     this.TileSize = tileSize;
@@ -56,33 +54,29 @@ export class MapView {
       height: tileSize * maze.height
     });
 
-    this.createMazeLayer(maze);
+    this.createMazeLayer();
     this.createObjectsLayer();
     this.createQLayer();
   }
-  
-  createMazeLayer(maze) {
+
+  createMazeLayer() {
     this.mapLayer = new Konva.Layer();
-    this.mapTiles = [maze.height];
-    for (var y = 0 ; y < maze.height ; y++) {
-      this.mapTiles[y] = [maze.width];
-      for (var x = 0 ; x < maze.width ; x++) {
-        const rect = new Konva.Rect({
-          x: x * this.TileSize,
-          y: y * this.TileSize,
-          width: this.TileSize,
-          height: this.TileSize,
-          fill: maze.isTransitable({ x: x, y: y}) ? '#FFFFFF' : '#101010'
-        });
-        this.mapTiles[y][x] = rect;
-        this.mapLayer.add(rect);
-      }
-    }
+    this.mapTiles = createMatrixFromMaze(this.maze);
+    this.maze.allCoordinates.forEach( coord => {
+      const rect = new Konva.Rect({
+        x: coord.x * this.TileSize,
+        y: coord.y * this.TileSize,
+        width: this.TileSize,
+        height: this.TileSize,
+        fill: this.maze.isTransitable({ x: coord.x, y: coord.y}) ? '#FFFFFF' : '#101010'
+      });
+      this.mapTiles[coord.y][coord.x] = rect;
+      this.mapLayer.add(rect);
+    });
     this.stage.add(this.mapLayer);
   }
 
   createImageAtTile(imageSource, coord) {
-    
     const thisImage = new Konva.Image({
       x: coord.x * this.TileSize,
       y: coord.y * this.TileSize,
@@ -152,148 +146,3 @@ export class MapView {
     this.objectsLayer.draw();
   }
 }
-
-export var MapBase = Vue.component('MapBase', {
-  props: ['machine', 'maze', 'config'],
-  data: function () {
-    return {
-      robot_image: null,
-      energy_image: null,
-    }
-  },
-  created() {
-    asyncLoadImage("img/robot.png", this.setRobotImage);
-    asyncLoadImage("img/station.png", this.setStationImage);
-  },
-  computed: {
-    main_config: function(){
-      return {
-        offset: {
-          x: -(this.config.width-this.base_size*this.maze.width)/2,
-          y: -(this.config.height-this.base_size*this.maze.height)/2,
-        }
-      }
-    },
-    robot_config: function() {
-      return {
-        height: this.base_size,
-        width: this.base_size,
-        x: this.center,
-        y: this.center,
-        image: this.robot_image,
-        offset:{
-          x: this.base_size/2,
-          y: this.base_size/2,
-        }
-      }
-    },
-    energy_config: function() {
-      return {
-        height: this.base_size,
-        width: this.base_size,
-        offset: {
-          x: this.base_size/2,
-          y: this.base_size/2
-        },
-        image: this.energy_image,
-      }
-    },
-    strokeW: function() {
-      return this.base_size / 50;
-    },
-    base_size: function() {
-      return Math.min(this.config.height/this.maze.height, this.config.width/this.maze.width);
-    },
-  },
-  methods: {
-    setRobotImage: function(image) {
-      this.robot_image = image;
-    },
-    setStationImage: function(image) {
-      this.energy_image = image;
-    },
-
-    get_tile_type: function(state) {
-      var pos = this.maze.state2position(state);
-      return maze.getTileType(pos);
-    },
-
-    get_field_config: function(state) {
-      var pos = this.maze.state2position(state);
-      return {
-        x: this.base_size * pos.x+this.base_size/2,
-        y: this.base_size * pos.y+this.base_size/2,
-      }
-    },
-    get_tile_config: function(t_type, index) {
-      return {
-        width: this.base_size,
-        height: this.base_size,
-        stroke: TileStrokeColor,
-        strokeWidth: this.strokeW,
-        offset: {
-          x: this.base_size/2,
-          y: this.base_size/2,
-        },
-        opacity: 1,
-        fill: occludedByFog(index) ? TileFogColor : getTileColor(t_type)
-      }
-    },
-    get_tile_value_config: function(index) {
-      return {
-        width: this.base_size,
-        height: this.base_size,
-        offset: {
-          x: this.base_size/2 + 4,
-          y: this.base_size/2 + 4,
-        },
-        opacity: ValueVisualizer.opacity(index),
-        fill: ValueVisualizer.fillColor(index)
-      }
-    }
-  }
-});
-
-function getTileColor(type) {
-  switch (type) {
-    case tile.regular:
-      return '#ffffff';
-    case tile.end:
-      return '#0eb500';
-    case tile.start:
-      return '#ff0008';
-    case tile.dangerous:
-      return '#FF7B17';
-    case tile.wall:
-      return '#000000';
-  }
-}
-
-
-Vue.component('rl-map', {
-  extends: MapBase,
-  computed: {
-    robot_config: function() {
-      return {
-        height: this.base_size,
-        width: this.base_size,
-        x: this.base_size * this.machine.state.x,
-        y: this.base_size * this.machine.state.y,
-        image: this.robot_image,
-      }
-    },
-  },
-  template:
-  `<v-stage ref="stage" :config="config">
-    <v-layer ref="map_layer" :config="main_config">
-      <v-group ref="map_group">
-        <v-group :key="'tile'+idx" v-for="(t_type, idx) in maze.map.flat()" :config="get_field_config(idx)">
-          <v-rect :config="get_tile_config(t_type, idx)"></v-rect>
-          <v-rect :config="get_tile_value_config(idx)" v-if="machine.show_qvalue_info"></v-rect>
-          <v-image :config="energy_config" v-if="t_type==8"></v-image>
-        </v-group>
-        <v-image :config="robot_config"></v-image>
-      </v-group>
-    </v-layer>
-  </v-stage>`
-})
