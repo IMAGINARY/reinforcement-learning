e0beff2096a5a5c31631e71da430dc641a1512e4
Sebastian Uribe <suribe@gmail.com>
2020-04-02 12:09:16 +0200

    Move Levels definition to another file

--- src/js/level.js
+++ src/js/level.js
@@ -1,4 +1,5 @@
-
+import { Texts } from "./language.js";
+import { machine } from './rl';
 
 export const LevelMaps = {
   Null: [
@@ -36,3 +37,101 @@ export const LevelMaps = {
     [2, 0, 0, 0, 1, 0, 1, 0, 0, 1]
   ]
 };
+
+export const Levels = {
+  letsMove: {
+    components: ["global"],
+    levelMap: [
+      [0, 0, 0, 0],
+      [0, 2, 0, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 0]
+    ],
+    infoBox: {
+      ...Texts.letsMove,
+      showState: true,
+      showActions: true,
+    },
+  },
+  findPower: {
+    components: ["global"],
+    hasFog: true,
+    levelMap: [
+      [8, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0],
+      [0, 1, 1, 0, 0],
+      [2, 0, 0, 0, 1],
+    ],
+    infoBox: {
+      ...Texts.findPower,
+      showState: true,
+      showActions: true
+    }
+  },
+  getRewarded: {
+    components: ["global"],
+    levelMap: [
+      [0, 0, 0, 0, 0],
+      [0, 1, 1, 1, 0],
+      [0, 1, 0, 1, 8],
+      [0, 1, 1, 1, 0],
+      [2, 0, 0, 0, 0]
+    ],
+    infoBox: {
+      ...Texts.getRewarded,
+      showState: true,
+      showActions: true,
+      showReward: true
+    }
+  },
+  accumulatedReward: {
+    components: ["global", "score"],
+    levelMap: [
+      [0, 0, 0, 0, 0],
+      [0, 1, 1, 1, 0],
+      [0, 1, 0, 1, 8],
+      [0, 1, 1, 1, 0],
+      [2, 0, 0, 0, 0]
+    ],
+    infoBox: {
+      ...Texts.accumulatedReward,
+      showState: true,
+      showActions: true,
+      showReward: true
+    }
+  },
+  playground: {
+    components: ["global", "sliders", "plot", "training","evaluation", "score", "editor"],
+    controls: ["learningRate", "discountFactor", "epsilon", "qvalue", "greedy"],
+    training: {
+      "Train 1 episode": () => machine.train(1),
+      "Train 20 episodes": () => machine.train(20),
+      "Unlearn all": () => machine.reset_machine(),
+      "Evaluate Robot": () => {
+        const evaluation = machine.evaluate(100);
+      }
+    },
+    evaluation: {
+      "Do 1 step": () => {
+        machine.learning = false;
+        machine.auto_step();
+        machine.learning = true;
+      },
+      "Do 1 greedy step": () => {
+        machine.learning = false;
+        machine.greedy_step();
+        machine.learning = true;
+      },
+    },
+    levelMap: LevelMaps.FullMap,
+    infoBox: {
+      title: 'Learning',
+      text: Texts.globalIntro,
+      showState: true,
+      showActions: true,
+      showReward: true
+    },
+  }
+};
+
--- src/js/view.js
+++ src/js/view.js
@@ -2,8 +2,7 @@ import Vue from 'vue';
 
 import { machine, maze, environment } from "./rl.js";
 import { setKeyboardActionCallback } from "./controls.js";
-import { Texts } from "./language.js";
-import { LevelMaps, Levels } from './level';
+import { Levels } from './level';
 
 import { MapView } from './map.js';
 import { renderEquation } from './equation.js';
@@ -39,103 +38,6 @@ var infoViews = {
   reward: false
 };
 
-export const StateMgr = {
-  letsMove: {
-    components: ["global"],
-    levelMap: [
-      [0, 0, 0, 0],
-      [0, 2, 0, 0],
-      [0, 0, 1, 0],
-      [0, 0, 0, 0]
-    ],
-    infoBox: {
-      ...Texts.letsMove,
-      showState: true,
-      showActions: true,
-    },
-  },
-  findPower: {
-    components: ["global"],
-    hasFog: true,
-    levelMap: [
-      [8, 0, 0, 0, 0],
-      [1, 1, 1, 1, 0],
-      [0, 0, 1, 0, 0],
-      [0, 1, 1, 0, 0],
-      [2, 0, 0, 0, 1],
-    ],
-    infoBox: {
-      ...Texts.findPower,
-      showState: true,
-      showActions: true
-    }
-  },
-  getRewarded: {
-    components: ["global"],
-    levelMap: [
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 1, 0, 1, 8],
-      [0, 1, 1, 1, 0],
-      [2, 0, 0, 0, 0]
-    ],
-    infoBox: {
-      ...Texts.getRewarded,
-      showState: true,
-      showActions: true,
-      showReward: true
-    }
-  },
-  accumulatedReward: {
-    components: ["global", "score"],
-    levelMap: [
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 1, 0],
-      [0, 1, 0, 1, 8],
-      [0, 1, 1, 1, 0],
-      [2, 0, 0, 0, 0]
-    ],
-    infoBox: {
-      ...Texts.accumulatedReward,
-      showState: true,
-      showActions: true,
-      showReward: true
-    }
-  },
-  playground: {
-    components: ["global", "sliders", "plot", "training","evaluation", "score", "editor"],
-    controls: ["learningRate", "discountFactor", "epsilon", "qvalue", "greedy"],
-    training: {
-      "Train 1 episode": () => machine.train(1),
-      "Train 20 episodes": () => machine.train(20),
-      "Unlearn all": () => machine.reset_machine(),
-      "Evaluate Robot": () => {
-        const evaluation = machine.evaluate(100);
-      }
-    },
-    evaluation: {
-      "Do 1 step": () => {
-        machine.learning = false;
-        machine.auto_step();
-        machine.learning = true;
-      },
-      "Do 1 greedy step": () => {
-        machine.learning = false;
-        machine.greedy_step();
-        machine.learning = true;
-      },
-    },
-    levelMap: LevelMaps.FullMap,
-    infoBox: {
-      title: 'Learning',
-      text: Texts.globalIntro,
-      showState: true,
-      showActions: true,
-      showReward: true
-    },
-  }
-};
-
 var app = new Vue({
   el: '#app',
   data: {
@@ -148,7 +50,7 @@ var app = new Vue({
     components: [],
     evaluation: {},
     training: {},
-    levels: Object.keys(StateMgr),
+    levels: Object.keys(Levels),
     currentLevel: null,
     editor: editor,
     message: {
@@ -213,7 +115,7 @@ var app = new Vue({
     },
 
     gotoLevel: function(levelName) {
-      const levelData = StateMgr[levelName];
+      const levelData = Levels[levelName];
 
       this.infoBox = levelData.infoBox;
       this.components = levelData.components;
