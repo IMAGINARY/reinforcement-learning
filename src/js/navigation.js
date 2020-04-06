63088075bf74fcd0274f37b64a3fcebd64cc207c
Sebastian Uribe <suribe@gmail.com>
2020-04-06 10:03:01 +0200

    Replace next/previous buttons for << and >>

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -13,9 +13,9 @@ Vue.component('navigationbar', {
   props: ["levels", "currentlevel"],
   template: `
   <nav class="buttonbar level-navigation">
-  <button class="nav-button" v-on:click="$emit('prev-level')">Previous</button>
+  <button class="nav-button" v-on:click="$emit('prev-level')">&lt;&lt;</button>
     <div v-bind:class="{ 'nav-current-level': (currentlevel == level) }" class="nav-circle" v-for="level in levels" v-on:click="$emit('goto-level', level)"></div>
-  <button class="nav-button" v-on:click="$emit('next-level')">Next</button>
+  <button class="nav-button" v-on:click="$emit('next-level')">&gt;&gt;</button>
   </nav>
   `
 });
