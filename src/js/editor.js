32c0f46b8d07044ba022ae7a6f823e80fe4bb42e
Sebastian Uribe <suribe@gmail.com>
2020-04-08 16:45:44 +0200

    rename styles for palette elements

--- src/js/editor.js
+++ src/js/editor.js
@@ -5,7 +5,7 @@ Vue.component('editor-palette', {
   template: `
   <div class="editor-palette">
     <button v-on:click="$emit('switch-editor')">Editor ON/OFF</button>
-    <div v-if="enabled" id="editor-current-type" :class="current_type"></div>
+    <div v-if="enabled" id="editor-current-type" :class="('palette_' + current_type)"></div>
     <button v-if="enabled" v-for="(type) in tile_types" v-on:click="$emit('set-tile-type', type)">{{ type }}</button>
   </div>`,
 });
