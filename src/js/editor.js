2fc044e329e80e1560a72f2a440bc223cb7e017c
Sebastian Uribe <suribe@gmail.com>
2020-04-08 16:30:30 +0200

    Re-arrange position of elements

--- src/js/editor.js
+++ src/js/editor.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 Vue.component('editor-palette', {
   props: ["tile_types", "current_type", "enabled"],
   template: `
-  <div id="editor-palette" class="editor-palette">
+  <div class="editor-palette">
     <button v-on:click="$emit('switch-editor')">Editor ON/OFF</button>
     <div v-if="enabled" id="editor-current-type" :class="current_type"></div>
     <button v-if="enabled" v-for="(type) in tile_types" v-on:click="$emit('set-tile-type', type)">{{ type }}</button>
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -12,7 +12,7 @@ Vue.component('buttonbar', {
 Vue.component('navigationbar', {
   props: ["levels", "currentlevel"],
   template: `
-  <nav class="buttonbar level-navigation">
+  <nav class="buttonbar">
   <button class="nav-button" v-on:click="$emit('prev-level')">&lt;&lt;</button>
     <div v-bind:class="{ 'nav-current-level': (currentlevel == level) }" class="nav-circle" v-for="level in levels" v-on:click="$emit('goto-level', level)"></div>
   <button class="nav-button" v-on:click="$emit('next-level')">&gt;&gt;</button>
